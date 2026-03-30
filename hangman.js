b3b83b77d616614ebe8f30219769065a81b2d518
Archayne <hargrovea31@gmail.com>
2026-03-30 10:16:39 -0400

    created new game logic

--- hangman.js
+++ hangman.js
@@ -1,3 +1,22 @@
-//1. Pick a word
+const POSSIBLE_WORDS = ["obdurate", "verisimilitude",
+    "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
+
+
+
+let newGame = function(){
+    //Pick a random word
+    let randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
+    let word = POSSIBLE_WORDS[randomIndex];
+    console.log("Word chosen: ", word);
+
+    //show the dashes
+    let clueString = "";
+    for(let i = 0; i < word.length; i++){
+        clueString += "_ ";
+    }
+
+    let clue = document.getElementById("clue");
+    clue.textContent = clueString;
+}
 //2.put dashes = word length
 //3 dfraw my starting hangman dude thing
