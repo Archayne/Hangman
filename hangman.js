93d2ef9eb811c21fd5c58705b1e12fd8117d8da9
Archayne <hargrovea31@gmail.com>
2026-03-30 10:41:02 -0400

    added letter guessing logic

--- hangman.js
+++ hangman.js
@@ -1,22 +1,40 @@
 const POSSIBLE_WORDS = ["obdurate", "verisimilitude",
     "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
-
-
+var word = "";
+var guesses = "";//this will be all the guesses
 
 let newGame = function(){
     //Pick a random word
     let randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
-    let word = POSSIBLE_WORDS[randomIndex];
-    console.log("Word chosen: ", word);
+    word = POSSIBLE_WORDS[randomIndex];
+    updatePage();
+}
 
-    //show the dashes
+let updatePage = function(){
     let clueString = "";
     for(let i = 0; i < word.length; i++){
-        clueString += "_ ";
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter) >= 0){
+            clueString += currentLetter + " ";
+        }else{
+            clueString += "_ ";
+        }
+        
     }
 
     let clue = document.getElementById("clue");
     clue.textContent = clueString;
+
+    let guessArea = document.getElementById("guesses");
+    guessArea.textContent = "Guesses: " + guesses;
 }
+
+let guessLetter = function(){
+    let input = document.getElementById("guess");
+    let letter = input.value;
+    guesses +=letter
+    updatePage();
+}
+
 //2.put dashes = word length
 //3 dfraw my starting hangman dude thing
